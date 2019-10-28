52046a591847328680db87af84ef48c4bba99aae
Alyssa Lundgren <lundgrea@gmail.com>
2019-10-28 09:59:40 -0600

    Move repl code into repo

--- index.js
+++ index.js
@@ -5,15 +5,31 @@
 
 //COMPARE ROWS
 //grab the first three indeces (0,1,2) and check for duplicates
+    //perhaps using a filter....
+    //thoughts about reduce...
 //flatten all three arrays
 //use array prototype method to loop through comparing elements
-//if duplicate return 'PLEASE TRY AGAIN'
+//if duplicate return 'Please try again.'
 //else increment indeces by 3 and run again
 //if no duplicated move on to comparing columns
 
 
-//COMPARE COLUMNS
 
+let findDups = (array) => {
+   return array.filter((item, index) => {
+     array.indexOf(item) != index
+     return 'Please play again'
+    })
+  }
+  
+
+//COMPARE COLUMNS
+//grab the first indeces of indeces 0,3,6,9,12,15,18,21,24 and compare for duplicates
+//if duplicates return 'Please try again.'
+//else compare the second indeces of the same arrays
+//compare the third indeces of the same arrays
+//increment indeces by 1 and run again
+//incremenet indeces by 1 and run again
 
 
 const isValidSudoku = () => {
