c948d6bcf1d42fa602d4e69dfc78b90450c4bc62
Alyssa Lundgren <lundgrea@gmail.com>
2019-10-28 09:42:45 -0600

    Begin pseudocode

---
+++ index.js
@@ -0,0 +1,30 @@
+//review rows for duplicates
+//review columns for duplicates
+//if no duplicates return 'SUCCESS!'
+//else return 'Please try again.'
+
+//COMPARE ROWS
+//grab the first three indeces (0,1,2) and check for duplicates
+//flatten all three arrays
+//use array prototype method to loop through comparing elements
+//if duplicate return 'PLEASE TRY AGAIN'
+//else increment indeces by 3 and run again
+//if no duplicated move on to comparing columns
+
+
+//COMPARE COLUMNS
+
+
+
+const isValidSudoku = () => {
+  
+} 
+
+const compareRows = () => {
+
+}
+
+const compareColumns = () => {
+
+}
+
